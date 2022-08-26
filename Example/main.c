40341a566aa0e182d7b4948ead300c7ffdcf61e5
mohammad mazarei <mohammad.mazarei@gmail.com>
2022-08-26 15:30:30 +0430

    fix test scenario

--- Example/main.c
+++ Example/main.c
@@ -11,55 +11,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <NorDB.h>
 #include <ll/Ram_ll.h>
 
 typedef struct  __attribute__((__packed__))
 {
-	uint32_t id;
-	uint8_t  dump[11];
+	uint8_t  dummy[11];
+	uint32_t checksum;
 }dummy_t;
 
+void get_RandomRecord(dummy_t *rec)
+{
+	rec->checksum = 0;
+	for(int i=0;i<11;i++)
+	{
+		rec->dummy[i] = rand() & 0xFF;
+		rec->checksum += rec->dummy[i];
+	}
+}
+
+bool check_bummyRecord(dummy_t *rec)
+{
+	uint32_t sum = 0;
+	for(int i=0;i<11;i++)
+	{
+		sum += rec->dummy[i];
+	}
+	return sum==rec->checksum;
+}
 
 int main(void)
 {
+	srand(time(NULL));
+
 	NorDB_HWLayer *RAM_Hw = Ramll_Init(512,2);
 	NorDB_t *DB = NorDB(RAM_Hw,sizeof(dummy_t));
 
+	/*add record to DB*/
 	dummy_t temp;
+	printf("Try insert 29 Record\n");
 	for(int i=0;i<29;i++)
 	{
-		temp.id = i;
-		memset(temp.dump,i,11);
+		get_RandomRecord(&temp);
 		uint32_t x = NorDB_AddRecord(DB, &temp);
-		printf("%i Add to %i\n",i,x);
+		if(x==0)
+		{
+			printf("Error to add Rec %i\n",i);
+			return 1;
+		}
 	}
 
+	/*mount another DB to This io layer*/
 	NorDB_t *DB_New = NorDB(RAM_Hw,sizeof(dummy_t));
 	printf("Unread Point is %i\n",NorDB_Get_TotalUnreadRecord(DB_New));
+	printf("Try insert 29 another Record\n");
 	for(int i=0;i<29;i++)
 	{
-		temp.id = i;
-		memset(temp.dump,i,11);
+		get_RandomRecord(&temp);
 		uint32_t x = NorDB_AddRecord(DB_New, &temp);
-		printf("%i Add to %i\n",i,x);
+		if(x==0)
+		{
+			printf("Error to add Rec %i\n",i);
+			return 1;
+		}
 	}
-	printf("Unread Point is %i\n",NorDB_Get_TotalUnreadRecord(DB));
+	uint32_t UNreadRecord = NorDB_Get_TotalUnreadRecord(DB);
+	printf("Total Unread Point is %i\n",UNreadRecord);
 
-	for(int i=0;i<29;i++)
+
+	for(int i=0;i<UNreadRecord;i++)
 	{
 		uint32_t x = NorDB_ReadRecord(DB_New, &temp);
-		printf("%i Read from %i -> %i\n",i,x,temp.id);
-	}
+		if(x==0)
+		{
+			printf("Error Read Reocrd\n");
+			return 1;
+		}
 
-	for(int i=0;i<1;i++)
-	{
-		temp.id = i;
-		memset(temp.dump,i,11);
-		uint32_t x = NorDB_AddRecord(DB, &temp);
-		printf("%i Add to %i\n",i,x);
+		if(check_bummyRecord(&temp)==false)
+		{
+			printf("Read Reocrd not Correct!\n");
+			return 1;
+		}
 	}
+	printf("We Read Back %d Record Correctly :)\n",UNreadRecord);
 
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
