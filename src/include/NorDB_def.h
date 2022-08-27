27e1932628258671228ac3b78ec02f3e36eed2f1
mohammad mazarei <mohammad.mazarei@gmail.com>
2022-08-27 12:26:36 +0430

    add commant

--- src/include/NorDB_def.h
+++ src/include/NorDB_def.h
@@ -7,18 +7,24 @@
 
 #ifndef NORDB_DEF_H_
 #define NORDB_DEF_H_
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
+#define NorDB_Magic	        0x0766
+#define NorDB_RVer	        0x0001
 
-#define NorDB_Magic	    0x0766
-#define NorDB_RVer	    0x0001
+#define nordb_FreeMark		0xFF
+#define nordb_UnReadMark	0xF0
+#define nordb_ReadMark		0x00
 
+/*heap and memory management*/
 #define nordb_malloc(x)	malloc(x)
 #define nordb_free(x)	free(x)
 #define nordb_memset	memset
 #define nordb_memcpy	memcpy
 
+/*file function*/
 
-#define nordb_FreeMark		0xFF
-#define nordb_UnReadMark	0xF0
-#define nordb_ReadMark		0x00
 #endif /* NORDB_DEF_H_ */
